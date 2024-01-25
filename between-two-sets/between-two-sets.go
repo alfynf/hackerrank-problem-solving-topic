dfba76c90cbc2505a526c8586434e85ebf934bde
alfynf <alfynurf36@gmail.com>
2024-01-25 09:47:30 +0700

    fix: minor modify

--- between-two-sets/between-two-sets.go
+++ between-two-sets/between-two-sets.go
@@ -5,26 +5,26 @@ import "fmt"
 func getTotalX(a []int32, b []int32) int32 {
 	minNum := a[0]
 	maxNum := b[len(b)-1]
-	
+
 	var countInt int32
 	for i := minNum; i <= maxNum; i++ {
 		isFirstConValid := true
 		isSecConValid := true
-		
+
 		// validate first condition
 		for j := 0; j < len(a); j++ {
-			if i % a[j] != 0 {
+			if i%a[j] != 0 {
 				isFirstConValid = false
-			}  
+			}
 		}
-		
+
 		// validate second condition
 		for k := 0; k < len(b); k++ {
-			if b[k] % i != 0 {
+			if b[k]%i != 0 {
 				isSecConValid = false
-			}  
+			}
 		}
-		
+
 		if isFirstConValid && isSecConValid {
 			countInt++
 		}
@@ -33,5 +33,5 @@ func getTotalX(a []int32, b []int32) int32 {
 }
 
 func main() {
-	fmt.Println(getTotalX([]int32{2,4}, []int32{16,32,96}))
+	fmt.Println(getTotalX([]int32{2, 4}, []int32{16, 32, 96}))
 }
--- breaking-the-record/breaking_the_record.go
+++ breaking-the-record/breaking_the_record.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func breakingRecords(scores []int32) []int32 {
-    var maxScore int32
-    var minScore int32
-    var countBreakMaxScore int32
-    var countBreakMinScore int32
-    
-    maxScore = scores[0]
-    minScore = scores[0]
-    
-    for i := 1; i < len(scores); i++ {
-        score := scores[i]
-        if (score < minScore) {
-            minScore = score
-            countBreakMinScore++
-        } else if (score > maxScore) {
-            maxScore = score
-            countBreakMaxScore++
-        }
-    }
-    
-    return []int32{countBreakMaxScore, countBreakMinScore}
+	var maxScore int32
+	var minScore int32
+	var countBreakMaxScore int32
+	var countBreakMinScore int32
+
+	maxScore = scores[0]
+	minScore = scores[0]
+
+	for i := 1; i < len(scores); i++ {
+		score := scores[i]
+		if score < minScore {
+			minScore = score
+			countBreakMinScore++
+		} else if score > maxScore {
+			maxScore = score
+			countBreakMaxScore++
+		}
+	}
+
+	return []int32{countBreakMaxScore, countBreakMinScore}
 }
 
 func main() {
-	breakingRecords([]int32{10, 5, 20, 20, 4, 5, 2, 25, 1})
+	fmt.Println(breakingRecords([]int32{10, 5, 20, 20, 4, 5, 2, 25, 1}))
 }
