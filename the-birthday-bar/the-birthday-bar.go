ff6798230c32315ecbd56d32da034bceed38e963
alfynf <alfynurf36@gmail.com>
2024-01-25 09:00:50 +0700

    add: the birthday bar

---
+++ the-birthday-bar/the-birthday-bar.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func birthday(s []int32, d int32, m int32) int32 {
+    var counter int32
+    for i := 0; i <= len(s)-int(m); i++ {
+        var sum int32
+        for j := i; j <= i+int(m)-1; j++ {
+            sum = sum + s[j]
+        }
+        if sum == d {
+            counter++
+        }
+    }
+    return counter
+}
+
+func main(){
+	fmt.Println(birthday([]int32{2,2,1,3,2},3,2))
+}
